9c37e11c889e1e05e20e4fe2482a289bf50713c6
Saem <fornared@gmail.com>
2024-06-05 17:52:40 +0900

    관광정보 - api 호출

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Home } from "./page/Home.jsx";
 import { BoardWrite } from "./page/board/BoardWrite.jsx";
 import { BoardList } from "./page/board/BoardList.jsx";
 import { BoardView } from "./page/board/BoardView.jsx";
+import { TourSearch } from "./page/tour/TourSearch.jsx";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +17,7 @@ const router = createBrowserRouter([
       { path: "board/write", element: <BoardWrite /> },
       { path: "board", element: <BoardList /> },
       { path: "board/:id", element: <BoardView /> },
+      { path: "tour", element: <TourSearch /> },
     ],
   },
 ]);
