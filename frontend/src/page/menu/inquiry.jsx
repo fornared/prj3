54d3297b375a13cb58285f9897c018ee2cf27498
sehyeon <gs1884@naver.com>
2024-06-12 16:53:07 +0900

    메뉴박스(질문, 1:1문의, 공지사항, 이벤트).jsx파일

--- frontend/src/page/menu/inquiry.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Inquiry(props) {
-  return <div></div>;
-}
-
-export default Inquiry;
--- frontend/src/page/menu/question.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Question(props) {
-  return <div></div>;
-}
-
-export default Question;
