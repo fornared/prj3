580f4bacdda8898b1fbffc3ec74325f5a8fa2853
sehyeon <gs1884@naver.com>
2024-06-13 12:09:15 +0900

    캐러셀 슬라이더 추가(디자인(예정) 추가)

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Box,
   Heading,
@@ -7,13 +8,24 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export function Home() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <Box>
       {/* 배너 섹션 */}
       <Box
-        bgImage="url('/path/to/your/banner-image.jpg')" //나중에 이미지 찾아서 넣을 예정
+        bgImage="url('/image/travel.jpg')" //나중에 이미지 찾아서 넣을 예정
         bgSize="cover"
         bgPosition="center"
         height="300px"
@@ -32,9 +44,8 @@ export function Home() {
           Welcome to Travel Place
         </Heading>
       </Box>
-
       {/* 메인 컨텐츠 */}
-      <Box p={4}>
+      <Box p={4} mb={8}>
         <VStack spacing={8} align="stretch">
           {/* 인기 여행지 섹션 */}
           <Box>
@@ -44,8 +55,7 @@ export function Home() {
             <SimpleGrid columns={{ base: 1, md: 3 }} spacing={4}>
               {/* 예시 여행지 카드 */}
               <Box borderWidth="1px" borderRadius="md" overflow="hidden">
-                {/*이미지 파일 찾아서 삽입예정*/}
-                <Image src="/path/to/image1.jpg" alt="여행지 1" />
+                <Image src="/image/image1.jpg" alt="여행지 1" />
                 <Box p={4}>
                   <Heading as="h3" size="md">
                     여행지 1
@@ -54,7 +64,7 @@ export function Home() {
                 </Box>
               </Box>
               <Box borderWidth="1px" borderRadius="md" overflow="hidden">
-                <Image src="/path/to/image2.jpg" alt="여행지 2" />
+                <Image src="/image/image2.jpg" alt="여행지 2" />
                 <Box p={4}>
                   <Heading as="h3" size="md">
                     여행지 2
@@ -63,8 +73,9 @@ export function Home() {
                 </Box>
               </Box>
               <Box borderWidth="1px" borderRadius="md" overflow="hidden">
-                <Image src="/path/to/image3.jpg" alt="여행지 3" />
+                <Image src="/image/image3.jpg" alt="여행지 3" />
                 <Box p={4}>
+                  <Heading as="h3" size="md"></Heading>
                   <Heading as="h3" size="md">
                     여행지 3
                   </Heading>
@@ -143,6 +154,33 @@ export function Home() {
                 </Box>
               </Box>
             </SimpleGrid>
+          </Box>
+
+          {/* 캐러셀 슬라이더 섹션 */}
+          <Box>
+            <Heading as="h2" size="xl" mb={4}>
+              스토리
+            </Heading>
+            <Slider {...settings}>
+              <Box p={4}>
+                <Image src="/image/slider-image1.jpg" alt="슬라이더 이미지 1" />
+                <Text mt={2}>이미지</Text>
+              </Box>
+              <Box p={4}>
+                <Image
+                  src="/path/to/slider-image2.jpg"
+                  alt="슬라이더 이미지 2"
+                />
+                <Text mt={2}>슬라이더 이미지 2 설명</Text>
+              </Box>
+              <Box p={4}>
+                <Image
+                  src="/path/to/slider-image3.jpg"
+                  alt="슬라이더 이미지 3"
+                />
+                <Text mt={2}>슬라이더 이미지 3 설명</Text>
+              </Box>
+            </Slider>
           </Box>
         </VStack>
 
